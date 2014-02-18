ad34c5953a805659b1023f121ef47e4aa9b5a844
Jeffrey Espiritu <jespiritu@myseneca.ca>
2014-02-17 22:56:46 -0500

    -has the option to choose which potion to brew -implements the potion making as per source code

--- alchemist.php
+++ alchemist.php
@@ -40,9 +40,24 @@
 					<td>Success Chance (%):</td>
 					<td><input type='text' name='per' /></td>
 				</tr>
+                <?php 
+                    $types = array('Red, Yellow, White Potion', 'Blue Potion', 
+                                   'Condensed Red Potion', 'Condensed Yellow Potion', 'Condensed White Potion');
+                                   
+					for ($i=0; $i<5; $i++) {
+						if ($i == 0)	// default selected value
+							print "<tr><td><input type='radio' name='potion' checked='checked' value='$i' />" . $types[$i] . "</td></tr>";
+						else
+							print "<tr><td><input type='radio' name='potion' value='$i' />" . $types[$i] . "</td></tr>";
+					}
+				?>
 				<tr>
 					<td>Amount:</td>
 					<td><input type='text' name='qty' /></td>
+				</tr>
+                <tr>
+					<td>Twilight:</td>
+					<td><input type='text' name='twilight' /></td>
 				</tr>
 			</table>
 
@@ -65,7 +80,10 @@ if (!isset($_POST["submit"]))
 
 $qty = $_POST["qty"];
 $per = $_POST["per"];
+$twilight = $_POST["twilight"];
 $nherbs = 0;    // # of herbs
+$potion = $_POST["potion"];
+$base_per = 0;
 
 $fame = 0;
 $pots = 0;
@@ -73,10 +91,14 @@ $counter = 0;
 
 $pointrate = 0;
 
+$types = array('Red, Yellow, White Potion', 'Blue Potion', 
+               'Condensed Red Potion', 'Condensed Yellow Potion', 'Condensed White Potion');
+
 $MAX_VALUE = 1000000;	// 1mil max
 
 // 0-100 | 000.00-100.00
 $isdecimal = "^[0-9]{1,3}$|^[0-9]{1,3}.[0-9]{1,2}$";
+$is_numeric = "[0-9]+";
 
 // validate fields
 
@@ -95,61 +117,118 @@ else if (!($qty <= $MAX_VALUE)) {
 	print "Integer overflow. Amount must be less than/equal to ". number_format($MAX_VALUE) ."<br />";
 	exit();
 }
+else if (!preg_match("/$is_numeric/", $twilight)) {
+	print "Error. ";
+	print "Twilight alchemy value must be a positive integer, > 0";
+	exit();
+}
 
 // remove decimal
 $per *= 100;
+$base_per = $per;
 
-for ($i=0; $i<$qty; $i++)
-{
-    $nherbs++; // # of white herbs consumed
-    if (rand(1, 10000) <= $per)
-    { // Success
-        $pots++;
-        //Add fame as needed.
-        switch (++$counter) {
-            case 3:
-                $fame+=1; // Success to prepare 3 Condensed Potions in a row
-                break;
-            case 5:
-                $fame+=3; // Success to prepare 5 Condensed Potions in a row
-                break;
-            case 7:
-                $fame+=10; // Success to prepare 7 Condensed Potions in a row
-                break;
-            case 10:
-                $fame+=50; // Success to prepare 10 Condensed Potions in a row
-                $counter = 0;
-                break;
-            default:
-                break;
-        }
-    } 
-	else { //Failure
-        $counter = 0;
+$rates = array(
+    // min, avg, max
+    array($per+2010, $per+2500, $per+3000),
+    array($per, $per, $per),
+    array($per, $per, $per),
+    array($per-500, $per-250, $per-10),  // Yellow slim
+    array($per-1000, $per-500, $per-10),  // White slim
+);
+
+// Apply caps
+for ($i=0; $i<5; $i++) {
+    for ($j=0; $j<3; $j++) {
+        if ($rates[$i][$j] < 1)
+            $rates[$i][$j] = 1;
+        else if ($rates[$i][$j] > 10000)
+            $rates[$i][$j] = 10000;
     }
 }
 
-$pointrate = sprintf("%.2f", $fame/$qty);
+// For Twilight Alchemy, the chance is calculated once per use.
+
+if ($potion == 4)
+    $qty = 200;
+else
+    $twilight = 1;
+
+for ($x=0; $x<$twilight; $x++) {
+    $per = $base_per;  // Restore the base percent.
+    
+    // Apply bonuses or penalties.
+    switch ($potion) {
+        case 0:  // Red, Yellow, White
+            $per += rand(1, 100)*10 + 2000;  // +2010 (+20.1%), +3000 (+30%)
+            break;
+        case 3:  // Condensed Yellow Potion
+            $per -= rand(1,50)*10;  // -10 (-0.1%), -500 (-5%)
+            break;
+        case 4:  // Condensed White Potion
+            $per -= rand(1,100)*10;  // -10 (-0.1%), -1000 (-10%)
+            break;
+        // No penalty
+        case 1:  // Blue Potion
+        case 2:  // Condensed Red Potion
+        default:
+            break;
+    }
+
+    for ($i=0; $i<$qty; $i++)
+    {
+        if (rand(1, 10000) <= $per)
+        { // Success
+            $pots++;
+            //Add fame as needed.
+            switch (++$counter) {
+                case 3:
+                    $fame+=1; // Success to prepare 3 Condensed Potions in a row
+                    break;
+                case 5:
+                    $fame+=3; // Success to prepare 5 Condensed Potions in a row
+                    break;
+                case 7:
+                    $fame+=10; // Success to prepare 7 Condensed Potions in a row
+                    break;
+                case 10:
+                    $fame+=50; // Success to prepare 10 Condensed Potions in a row
+                    $counter = 0;
+                    break;
+                default:
+                    break;
+            }
+        }
+        else { //Failure
+            $counter = 0;
+        }
+    }
+}
+
+$pointrate = sprintf("%.2f", $fame/($qty*$twilight));
 
 print "<table border='1'>".
       "<tr>".
+      "   <td>Type</td>".
+      "   <td>". $types[$potion] ."</td>".
+      "</tr>".
+      "<tr>".
       "   <td>Potions Made</td>".
       "   <td>". number_format($pots) ."</td>".
+      "</tr>".
+      "<tr>".
+      "   <td>Attempts</td>".
+      "   <td>". number_format($qty) ." / ". number_format($twilight) ."</td>".
       "</tr>".
       "<tr>".
       "   <td>Fame Points</td>".
       "   <td>". number_format($fame) ."</td>".
       "</tr>".
       "<tr>".
-      "   <td>Herbs Used</td>".
-      "   <td>". number_format($nherbs) ."</td>".
-      "</tr>".
-      "<tr>".
       "   <td>Success Rate (%)</td>".
-      "   <td>". number_format($per/100, 2) ."</td>".
+      "   <td>". sprintf("%.2f%% %.2f%% %.2f%%", $rates[$potion][0]/100, $rates[$potion][1]/100, $rates[$potion][2]/100) ."</td>".
       "</tr>".
 	  "<tr>".
-      "   <td>Fame Points/Herb</td>".
+      "   <td>Fame Points/Attempts</td>".
       "   <td>". $pointrate ."</td>".
       "</tr>".
 	  "</table>";
