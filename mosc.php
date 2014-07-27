4c31b27ea1ea7dc2667ce205b333de1176715a01
Jeffrey Espiritu <jeff_pro3@hotmail.com>
2014-07-26 20:09:36 -0400

    Incorporates success chance into Blue Herb to zeny conversion -Iterates through the data hash without using a separate keys array

--- mosc.php
+++ mosc.php
@@ -22,6 +22,10 @@
         <td>Blue Herb: </td>
         <td><input type="text" name="bherbs"/> </td>
     </tr>
+	<tr>
+		<td>Success Chance (%)</td>
+		<td><input type="text" name="per" value="99.00"/></td>
+	</tr>
 </table>
     
 <br />
@@ -35,19 +39,25 @@
 if (!isset($_POST["submit"]))
 	exit();
 
-$total = 0;
+$per = $_POST["per"];
 
+$isdecimal = "^([0-9]{1,3}|[0-9]{1,3}\\.[0-9]{1,2})$";
+
+if (!preg_match("/$isdecimal/", $per) || $per > 100.00) {
+	print "Error. ";
+	print "Success chance must be in format ###.## and in range 0.00-100.00<br />";
+	exit();
+}
+
+$total = 0;
 $skilllvl = 10;
 $overcharge = array(7, 9, 11, 13, 15, 17, 19, 21, 23, 24);
-
-$keys = array("mirrors", "roses", "bherbs");
-
 $data = array(
             "mirrors" => array($_POST["mirrors"], "Crystal Mirror", "/images/747.gif", 7500),
             "roses"   => array($_POST["roses"], "Witherless Rose", "/images/748.gif", 27500),
-			"bherbs"  => array($_POST["bherbs"], "Blue Herb", "/images/510.gif", 3100),	// 3100 - (319 + 10 + 6)
-                                                                                        // 3100 - 335
-                                                                                        // 2765
+			"bherbs"  => array($_POST["bherbs"], "Blue Herb", "/images/510.gif", 2500),	// 3100 - (319 + 7 + 6)
+                                                                                        // 3100 - 332
+                                                                                        // 2768
         );
         
 print "<table border='1' cellpadding='10' >" .
@@ -55,23 +65,23 @@ print "<table border='1' cellpadding='10' >" .
       "<th>Item</th><th>Amount</th><th>Charge</th><th>Zeny</th>" .
       "</tr>";
 
-foreach ($keys as $k=>$v) {
-    # round value with sprintf
-    $value = sprintf("%d", $data[$v][3] * (100 + $overcharge[$skilllvl-1]) / 100);  # overcharge value
-    $value -= ($v == "bherbs") ? 335 : 0;
-    $zeny = $value * $data[$v][0];   # amount * overcharge value
+foreach ($data as $k=>$item) {
+    // round value with sprintf
+    $value = sprintf("%d", $item[3] * (100 + $overcharge[$skilllvl-1]) / 100);  // overcharge value
+    $value -= ($k == "bherbs") ? ($value*(100-$per)/100 + 332) : 0;  // factor in brewing chance deduction
+    $zeny = $item[0] * $value;  // amount * final value
     $total += $zeny;
     
-    $amount = ($data[$v][0] > 0) ? $data[$v][0] : '&nbsp;'; # show empty cell?
+    $amount = ($item[0] > 0) ? $item[0] : '&nbsp;'; # show empty cell?
     $zenyfm = "&nbsp;";
     
     if ($zeny > 0)
-        $zenyfm = number_format($zeny);    # format_zeny($zeny);
+        $zenyfm = number_format($zeny);
     
     print "<tr>" .
-          "<td><img src='" . $data[$v][2] . "' alt='" . $data[$v][1] . "' width='24' height='24' /></td>" .
+          "<td><img src='" . $item[2] . "' alt='" . $item[1] . "' width='24' height='24' /></td>" .
           "<td>$amount</td>" .
-          "<td>". number_format($value) ."</td>" .
+          "<td>". (($k=="bherbs")?"2500*1.24-332=":"") . number_format($value) ."</td>" .
           "<td>$zenyfm</td>" .
           "</tr>";
 }
